03374d44e54ce1476de4000bc210c7f02a458621
harshitguptafingermark <harshit.gupta@fingermarkglobal.com>
2022-12-01 19:31:14 +0530

    update

--- snapme_frontend/src/components/feed.tsx
+++ snapme_frontend/src/components/feed.tsx
@@ -1,5 +1,41 @@
-function Feed() {
-  return <div>Feed</div>;
-}
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+import { client } from "../client";
+import { feedQuery, searchQuery } from "../utils/data";
+import MasonryLayout from "./masonry.layout";
+import Spinner from "./spinner";
+
+const Feed = () => {
+  const [pins, setPins] = useState();
+  const [loading, setLoading] = useState(false);
+  const { categoryId } = useParams();
+
+  useEffect(() => {
+    if (categoryId) {
+      setLoading(true);
+      const query = searchQuery(categoryId);
+      client.fetch(query).then((data) => {
+        setPins(data);
+        setLoading(false);
+      });
+    } else {
+      setLoading(true);
+
+      client.fetch(feedQuery).then((data) => {
+        setPins(data);
+        setLoading(false);
+      });
+    }
+  }, [categoryId]);
+  const ideaName = categoryId || "new";
+  console.log(pins);
+  if (loading) {
+    return (
+      <Spinner message={`We are adding ${ideaName} ideas to your feed!`} />
+    );
+  }
+  return <div>{pins && <MasonryLayout pins={pins} />}</div>;
+};
 
 export default Feed;
